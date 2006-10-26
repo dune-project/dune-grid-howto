b59def1173212b56fdca76fb6d1911a1c3c4bf17
Robert Klöfkorn <robertk@dune-project.org>
2006-10-26 09:10:30 +0000

    quick fix ALUGrid and VTK Writer.
    [[Imported from SVN: r86]]

--- visualization.cc
+++ visualization.cc
@@ -7,10 +7,10 @@
 #include <iomanip>
 #include <stdio.h>
 
-#include "unitcube.hh"
-#include "functors.hh"
 #include "elementdata.hh"
 #include "vertexdata.hh"
+#include "functors.hh"
+#include "unitcube.hh"
 
 //! supply functor
 template<class Grid>
@@ -20,7 +20,7 @@ void dowork (Grid& grid)
   Exp<typename Grid::ctype,Grid::dimension> f;
 
   // refine the grid
-  grid.globalRefine(5);
+  grid.globalRefine(1);
 
   // call the visualization functions
   elementdata(grid,f);
@@ -33,22 +33,29 @@ int main(int argc, char **argv)
   MPI_Init(&argc,&argv);
 #endif
 
-  UnitCube<Dune::OneDGrid<1,1>,1> uc0;
-  UnitCube<Dune::YaspGrid<3,3>,1> uc1;
-  UnitCube<Dune::YaspGrid<2,2>,1> uc2;
-  UnitCube<Dune::SGrid<1,1>,1> uc3;
+  /*
+     UnitCube<Dune::OneDGrid<1,1>,1> uc0;
+     UnitCube<Dune::YaspGrid<3,3>,1> uc1;
+     UnitCube<Dune::YaspGrid<2,2>,1> uc2;
+     UnitCube<Dune::SGrid<1,1>,1> uc3;
+     UnitCube<Dune::SGrid<2,2>,1> uc4;
+     UnitCube<Dune::SGrid<3,3>,1> uc5;
+     #if HAVE_UG
+     UnitCube<Dune::UGGrid<3,3>,2> uc6;
+     #endif
+     #if HAVE_ALBERTA
+     #if DUNE_PROBLEM_DIM==2
+     UnitCube<Dune::AlbertaGrid<2,2>,1> uc7;
+     #endif
+     #endif
+   */
   UnitCube<Dune::SGrid<2,2>,1> uc4;
-  UnitCube<Dune::SGrid<3,3>,1> uc5;
-#if HAVE_UG
-  UnitCube<Dune::UGGrid<3,3>,2> uc6;
-#endif
-#if HAVE_ALBERTA
-#if DUNE_PROBLEM_DIM==2
-  UnitCube<Dune::AlbertaGrid<2,2>,1> uc7;
-#endif
-#endif
+  dowork(uc4.grid());
 
-  dowork(uc1.grid());
+#if HAVE_ALUGRID
+  UnitCube<Dune::ALUSimplexGrid<3,3> ,1> uc8;
+  dowork(uc8.grid());
+#endif
 
 #if HAVE_MPI
   MPI_Finalize();
