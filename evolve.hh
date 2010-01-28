886da845a6d4ffdcd83dcf9bc3c6fbae0053b853
Martin Drohmann <mdroh_01@dune-project.org>
2010-01-28 14:40:08 +0000

    backport patch 277 from trunk: update to finitevolume example
    use the new center, centerUnitOuterNormal methods() of Dune::Geometry
    
    [[Imported from SVN: r281]]

--- evolve.hh
+++ evolve.hh
@@ -1,6 +1,5 @@
 // -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
 // vi: set et ts=4 sw=2 sts=2:
-#include <dune/grid/common/referenceelements.hh>
 
 template<class G, class M, class V>
 void evolve (const G& grid, const M& mapper, V& c, double t, double& dt)
@@ -18,8 +17,14 @@ void evolve (const G& grid, const M& mapper, V& c, double t, double& dt)
   // element iterator type
   typedef typename GridView::template Codim<0>::Iterator LeafIterator;
 
+  // leaf entity geometry
+  typedef typename LeafIterator::Entity::Geometry LeafGeometry;
+
   // intersection iterator type
   typedef typename GridView::IntersectionIterator IntersectionIterator;
+
+  // intersection geometry
+  typedef typename IntersectionIterator::Intersection::Geometry IntersectionGeometry;
 
   // entity pointer type
   typedef typename G::template Codim<0>::EntityPointer EntityPointer;
@@ -38,20 +43,12 @@ void evolve (const G& grid, const M& mapper, V& c, double t, double& dt)
   LeafIterator endit = gridView.template end<0>();     /*@\label{evh:loop0}@*/
   for (LeafIterator it = gridView.template begin<0>(); it!=endit; ++it)
   {
-    // cell geometry type
-    Dune::GeometryType gt = it->type();
+    // cell geometry
+    const LeafGeometry &gt = it->geometry();
 
-    // cell center in reference element
-    const Dune::FieldVector<ct,dim>&
-    local = Dune::ReferenceElements<ct,dim>::general(gt).position(0,0);
-
-    // cell center in global coordinates
-    Dune::FieldVector<ct,dimworld>
-    global = it->geometry().global(local);
 
     // cell volume, assume linear map here
-    double volume = it->geometry().integrationElement(local)
-                    *Dune::ReferenceElements<ct,dim>::general(gt).volume();
+    double volume = gt.volume();
 
     // cell index
     int indexi = mapper.map(*it);
@@ -64,20 +61,15 @@ void evolve (const G& grid, const M& mapper, V& c, double t, double& dt)
     for (IntersectionIterator is = gridView.ibegin(*it); is!=isend; ++is)
     {
       // get geometry type of face
-      Dune::GeometryType gtf = is->type();
-
-      // center in face's reference element
-      const Dune::FieldVector<ct,dim-1>&
-      facelocal = Dune::ReferenceElements<ct,dim-1>::general(gtf).position(0,0);
+      const IntersectionGeometry &gtf = is->geometry();
 
       // get normal vector scaled with volume
       Dune::FieldVector<ct,dimworld> integrationOuterNormal
-        = is->integrationOuterNormal(facelocal);
-      integrationOuterNormal
-        *= Dune::ReferenceElements<ct,dim-1>::general(gtf).volume();
+        = is->centerUnitOuterNormal();
+      integrationOuterNormal *= gtf.volume();
 
       // center of face in global coordinates
-      Dune::FieldVector<ct,dimworld> faceglobal = is->geometry().global(facelocal);
+      Dune::FieldVector<ct,dimworld> faceglobal = gtf.center();
 
       // evaluate velocity at face center
       Dune::FieldVector<double,dim> velocity = u(faceglobal,t);
@@ -101,11 +93,8 @@ void evolve (const G& grid, const M& mapper, V& c, double t, double& dt)
              (it->level()==outside->level() && indexi<indexj) )
         {
           // compute factor in neighbor
-          Dune::GeometryType nbgt = outside->type();
-          const Dune::FieldVector<ct,dim>&
-          nblocal = Dune::ReferenceElements<ct,dim>::general(nbgt).position(0,0);
-          double nbvolume = outside->geometry().integrationElement(nblocal)
-                            *Dune::ReferenceElements<ct,dim>::general(nbgt).volume();
+          const LeafGeometry &nbgt = outside->geometry();
+          double nbvolume = nbgt.volume();
           double nbfactor = velocity*integrationOuterNormal/nbvolume;
 
           if (factor<0)                         // inflow
--- initialize.hh
+++ initialize.hh
@@ -1,6 +1,5 @@
 // -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
 // vi: set et ts=4 sw=2 sts=2:
-#include <dune/grid/common/referenceelements.hh>
 
 //! initialize the vector of unknowns with initial value
 template<class G, class M, class V>
@@ -19,6 +18,9 @@ void initialize (const G& grid, const M& mapper, V& c)
   // leaf iterator type
   typedef typename GridView::template Codim<0>::Iterator LeafIterator;
 
+  // geometry type
+  typedef typename LeafIterator::Entity::Geometry Geometry;
+
   // get grid view on leaf part
   GridView gridView = grid.leafView();
 
@@ -26,16 +28,11 @@ void initialize (const G& grid, const M& mapper, V& c)
   LeafIterator endit = gridView.template end<0>();
   for (LeafIterator it = gridView.template begin<0>(); it!=endit; ++it)
   {
-    // get geometry type
-    Dune::GeometryType gt = it->type();
-
-    // get cell center in reference element
-    const Dune::FieldVector<ct,dim>&
-    local = Dune::ReferenceElements<ct,dim>::general(gt).position(0,0);
+    // get geometry
+    const Geometry &gt = it->geometry();
 
     // get global coordinate of cell center
-    Dune::FieldVector<ct,dimworld> global =
-      it->geometry().global(local);
+    Dune::FieldVector<ct,dimworld> global = gt.center();
 
     // initialize cell concentration
     c[mapper.map(*it)] = c0(global);
