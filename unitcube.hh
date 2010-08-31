213fa267bf55d0644a67f97be8a9208111455e54
Oliver Sander <sander@dune-project.org>
2010-08-31 09:41:03 +0000

    Make the default implementation of the UnitCube class use the StructuredGridFactory, instead just throwing an exception and expecting all work to be done in specializations.
    [[Imported from SVN: r324]]

--- unitcube.hh
+++ unitcube.hh
@@ -5,6 +5,7 @@
 
 #include <dune/common/exceptions.hh>
 #include <dune/common/fvector.hh>
+#include <dune/grid/utility/structuredgridfactory.hh>
 
 // default implementation for any template parameter
 template<typename T, int variant>                      /*@\label{uc:uc0}@*/
@@ -13,20 +14,37 @@ class UnitCube
 public:
   typedef T GridType;
 
+  static const int dim = GridType::dimension;
+
   // constructor throwing exception
   UnitCube ()
   {
-    DUNE_THROW(Dune::Exception,"no specialization for this grid available");
+    Dune::FieldVector<typename GridType::ctype,dim> lowerLeft(0);
+    Dune::FieldVector<typename GridType::ctype,dim> upperRight(1);
+    Dune::array<unsigned int,dim> elements;
+    std::fill(elements.begin(), elements.end(), 1);
+
+    switch (variant) {
+    case 1 :
+      grid_ = Dune::StructuredGridFactory<GridType>::createCubeGrid(lowerLeft, upperRight, elements);
+      break;
+    case 2 :
+      grid_ = Dune::StructuredGridFactory<GridType>::createSimplexGrid(lowerLeft, upperRight, elements);
+      break;
+    default :
+      DUNE_THROW( Dune::NotImplemented, "Variant "
+                  << variant << " of unit cube not implemented." );
+    }
   }
 
   T& grid ()
   {
-    return grid_;
+    return *grid_;
   }
 
 private:
   // the constructed grid object
-  T grid_;
+  Dune::shared_ptr<T> grid_;
 };                                                     /*@\label{uc:uc1}@*/
 
 // include basic unitcube using GridFactory concept
